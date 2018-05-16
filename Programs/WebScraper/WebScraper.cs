6630e567576613f45841cadec2b790fba48fbba7
Peringer <alexis.peringer@sap.com>
2018-05-16 15:27:54 +0200

    Web Scraper

---
+++ Programs/WebScraper/WebScraper.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace clCrawler2
+{
+    public class ScrapeCriteriaPart
+    {
+        public string Regex { get; set; }
+        public RegexOptions RegexOption { get; set; }
+    }
+
+    public class ScrapeCriteria
+    {
+        public ScrapeCriteria()
+        {
+            Parts = new List<ScrapeCriteriaPart>();
+        }
+
+        public string Data { get; set; }
+        public string Regex { get; set; }
+        public RegexOptions RegexOption { get; set; }
+        public List<ScrapeCriteriaPart> Parts { get; set; }
+    }
+
+
+    public class ScrapeCriteriaBuilder
+    {
+        private string _data;
+        private string _regex;
+        private RegexOptions _regexOption;
+        private List<ScrapeCriteriaPart> _parts;
+
+        public void SetDefaults()
+        {
+            _data = string.Empty;
+            _regex = string.Empty;
+            _regexOption = RegexOptions.None;
+            _parts = new List<ScrapeCriteriaPart>();
+        }
+
+        public ScrapeCriteriaBuilder()
+        {
+            SetDefaults();
+        }
+
+        public ScrapeCriteriaBuilder WithData(string data)
+        {
+            _data = data;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithRegex(string regex)
+        {
+            _regex = regex;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithRegexOption(RegexOptions regexOption)
+        {
+            _regexOption = regexOption;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithParts(ScrapeCriteriaPart part)
+        {
+            _parts.Add(part);
+            return this;
+        }
+
+        public ScrapeCriteria Build()
+        {
+            ScrapeCriteria scrapeCriteria = new ScrapeCriteria();
+            scrapeCriteria.Data = _data;
+            scrapeCriteria.Regex = _regex;
+            scrapeCriteria.RegexOption = _regexOption;
+            scrapeCriteria.Parts = _parts;
+
+            return scrapeCriteria;
+
+        }
+
+    }
+
+    public class ScrapeCriteriaPartBuilder
+    {
+        private string _regex;
+        private RegexOptions _regexOption;
+
+        private void SetDefaults()
+        {
+            _regex = string.Empty;
+            _regexOption = RegexOptions.None;
+        }
+
+        public ScrapeCriteriaPartBuilder()
+        {
+            SetDefaults();
+        }
+
+        public ScrapeCriteriaPartBuilder WithRegex(string regex)
+        {
+            _regex = regex;
+            return this;
+        }
+
+        public ScrapeCriteriaPartBuilder WithRegexOption(RegexOptions regexOption)
+        {
+            _regexOption = regexOption;
+            return this;
+        }
+
+        public ScrapeCriteriaPart Build()
+        {
+            ScrapeCriteriaPart scrapeCriteriaPart = new ScrapeCriteriaPart();
+            scrapeCriteriaPart.Regex = _regex;
+            scrapeCriteriaPart.RegexOption = _regexOption;
+            return scrapeCriteriaPart;
+        }
+
+    }
+
+    public class Scraper
+    {
+        public List<string> Scrape(ScrapeCriteria scrapeCriteria)
+        {
+            List<string> scrapedElements = new List<string>();
+
+            MatchCollection matches = Regex.Matches(scrapeCriteria.Data, scrapeCriteria.Regex, scrapeCriteria.RegexOption);
+
+            foreach (Match match in matches)
+            {
+                if (!scrapeCriteria.Parts.Any())
+                {
+                    scrapedElements.Add(match.Groups[0].Value);
+                }
+                else
+                {
+                    foreach (var part in scrapeCriteria.Parts)
+                    {
+                        Match matchedPart = Regex.Match(match.Groups[0].Value, part.Regex, part.RegexOption);
+
+                        if (matchedPart.Success)
+                        {
+                            scrapedElements.Add(matchedPart.Groups[1].Value);
+                        }
+                    }
+                }
+            }
+
+            return scrapedElements;
+
+        }
+    }
+
+    class Program
+    {
+        private const string Method = "search";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a city: ");
+                var city = Console.ReadLine() ?? string.Empty;
+
+                Console.Write("Enter a category: ");
+                var category = Console.ReadLine() ?? string.Empty;
+
+                using (WebClient client = new WebClient())
+                {
+                    string content = client.DownloadString($"http://{city.Replace(" ", string.Empty)}.craigslist.org/{Method}/{category}");
+
+                    ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+                        .WithData(content)
+                        .WithRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
+                        .WithRegexOption(RegexOptions.ExplicitCapture)
+                        .WithParts(new ScrapeCriteriaPartBuilder()
+                            .WithRegex(@">(.*?)</a>")
+                            .WithRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .WithParts(new ScrapeCriteriaPartBuilder()
+                            .WithRegex(@"href=\""(.*?)\""")
+                            .WithRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .Build();
+
+                    Scraper scraper = new Scraper();
+
+                    var scrapedElements = scraper.Scrape(scrapeCriteria);
+
+                    if (scrapedElements.Any())
+                    {
+                        foreach (var scrapedElement in scrapedElements)
+                        {
+                            Console.WriteLine(scrapedElement);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No match.");
+                    }
+
+
+                    Console.ReadKey();
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
